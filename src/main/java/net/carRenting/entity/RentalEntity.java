98e147cadc53b3f82bac22a5caed9a170b9a02ac
Jaime Rosello <jarogos14@gmail.com>
2023-11-22 13:23:51 +0100

    Quitadas importaciones innecesarias

--- src/main/java/net/carRenting/entity/RentalEntity.java
+++ src/main/java/net/carRenting/entity/RentalEntity.java
@@ -1,16 +1,12 @@
 package net.carRenting.entity;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 
